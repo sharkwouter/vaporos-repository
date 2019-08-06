ec9603545ce28ddc198d205423ed7736d897bf0e
Wouter Wijsman <wwijsman@live.nl>
2019-08-06 12:34:39 +0200

    Added source files

--- update-repo.sh
+++ update-repo.sh
@@ -6,4 +6,10 @@ PACKAGEDIR="${PWD}/packages"
 REPODIR="${PWD}/repo"
 DISTNAME="buster"
 
+#add deb packages
 reprepro -Vb ${REPODIR} includedeb ${DISTNAME} ${PACKAGEDIR}/*.deb
+
+#include source files
+for dsc in `ls ${PACKAGEDIR}/*.dsc`; do
+	reprepro -Vb ${REPODIR} includedsc ${DISTNAME} ${dsc}
+done
